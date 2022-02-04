fedb52f5c2fc7b006479cb20504efbb5dbbb4b5a
Lenovo <makhmudov.kay@gmail.com>
2022-02-04 14:02:09 +0500

    app.routing resolved; add language param to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AppComponent } from './components/app/app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
 
 const routes: Routes = [
   {
@@ -10,27 +11,46 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        component: HomeComponent
+        redirectTo: 'ru',
+        pathMatch: 'full'
+      },
+      {
+        path: ':language',
+        component: LayoutComponent,
+        children: [
+          {
+            path: '',
+            component: HomeComponent,
+          },
+          {
+            path: 'market',
+            loadChildren: () =>
+              import('./modules/market/market.module').then(
+                (m) => m.MarketModule
+              ),
+          },
+          {
+            path: 'online-consultant',
+            loadChildren: () =>
+              import(
+                './modules/online-consultant/online-consultant.module'
+              ).then((m) => m.OnlineConsultantModule),
+          },
+        ],
       },
       {
         path: 'agro-id',
-        loadChildren: () => import('./modules/agro-id/agro-id.module').then(m => m.AgroIdModule)
+        loadChildren: () =>
+          import('./modules/agro-id/agro-id.module').then(
+            (m) => m.AgroIdModule
+          ),
       },
-      {
-        path: 'market',
-        loadChildren: () => import('./modules/market/market.module').then(m => m.MarketModule)
-      },
-      {
-        path: 'online-consultant',
-        loadChildren: () => import('./modules/online-consultant/online-consultant.module')
-          .then(m => m.OnlineConsultantModule)
-      },
-    ]
-  }
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+const DEFAULT_LANGUAGE = 'ru';
+const LOCAL_STORAGE_LANGUAGE = 'language';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
   title = 'agro-zamin';
+  param = {value: 'world'};
+
+  constructor(translate: TranslateService) {
+    let curentLanguage = localStorage.getItem(LOCAL_STORAGE_LANGUAGE);
+    if (!curentLanguage) {
+      curentLanguage = DEFAULT_LANGUAGE;
+      localStorage.setItem(LOCAL_STORAGE_LANGUAGE, curentLanguage);
+    }
+    translate.setDefaultLang(DEFAULT_LANGUAGE);
+    translate.use(curentLanguage);
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+  }
 }
