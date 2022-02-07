6273fbc8866b0e94e6445290ac637d72956a5a49
utamuratov <43562558+utamuratov@users.noreply.github.com>
2022-02-07 11:44:12 +0500

    core changed to shared

--- src/app/modules/online-consultant/online-consultant.module.ts
+++ src/app/modules/online-consultant/online-consultant.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OnlineConsultantComponent } from './components/online-consultant/online-consultant.component';
 import { OnlineConsultantRoutes } from './online-consultant.routing';
-import { SharedeModule } from 'src/app/shared/core.module';
+import { SharedeModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
